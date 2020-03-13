3da4200b2fbe70e2e66bcf2374b661484f3080b9
Pavel Poltaves <dzhura17@yandex.ru>
2020-03-13 15:46:04 +0200

    Start from the begining

---
+++ html/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(html.Startup))]
+
+namespace html
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+          /*  app.Run(context =>
+            {
+                if (context.Request.Path.Value == "/test")
+                {
+                context.Response.StatusCode = 404;
+                return Task.Delay(0); 
+                }
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("TEST");
+            });*/
+
+        }
+    }
+}
