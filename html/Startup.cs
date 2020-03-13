41b3b2ef6101d950841671ab716ea2b4c6c1bd0e
Pavel Poltaves <dzhura17@yandex.ru>
2020-03-13 16:37:07 +0200

    Login form

--- html/Startup.cs
+++ html/Startup.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(html.Startup))]
@@ -13,17 +14,22 @@ namespace html
     {
         public void Configuration(IAppBuilder app)
         {
-            
-          /*  app.Run(context =>
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                if (context.Request.Path.Value == "/test")
-                {
-                context.Response.StatusCode = 404;
-                return Task.Delay(0); 
-                }
-                context.Response.ContentType = "text/plain";
-                return context.Response.WriteAsync("TEST");
-            });*/
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login"),
+            });
+            /*  app.Run(context =>
+              {
+                  if (context.Request.Path.Value == "/test")
+                  {
+                  context.Response.StatusCode = 404;
+                  return Task.Delay(0); 
+                  }
+                  context.Response.ContentType = "text/plain";
+                  return context.Response.WriteAsync("TEST");
+              });*/
 
         }
     }
